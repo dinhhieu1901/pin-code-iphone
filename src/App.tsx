7923f5c46c707ebf88034e0a0819275441f01742
dinhhieu1901 <dinhhieu.nguyen@nanofin.tech>
2023-04-20 10:48:53 +0700

    chore: create-porject

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import LockScreenPin from "./components/LockScreenPin";
+
+function App() {
+  return <LockScreenPin />;
+}
+
+export default App;
---
+++ src/components/LockScreenPin.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LockScreenPin = () => {
+  return <div>LockScreenPin</div>;
+};
+
+export default LockScreenPin;
